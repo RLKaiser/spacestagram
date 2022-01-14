1208da6aae44f405b9eb5d9c285fe7365cc02381
Richelle Kaiser <kaiser.richelle@gmail.com>
2022-01-14 15:34:22 -0700

    breaking up fetching data and displaying to DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
-(function() {
-    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=1dqCkXhcDym7QqNzSb1hpgOjBndEiaOaoD7y2IwJ')
-    .then(res => res.json())
-    .then(data => {
-        let spacePhotos = data.photos;
-        console.log(spacePhotos);
-        for (let i = 0; i < spacePhotos.length; i++) {
-            let template = `
-            <div>
-                <h2>${spacePhotos[i].rover.name} Rover - ${spacePhotos[i].camera.full_name}</h2>
-                <img src="${spacePhotos[i].img_src}" alt="${spacePhotos[i].camera.full_name}">
-                <p>${spacePhotos[i].earth_date}</p>
-            </div>
-            `
-            document.querySelector('main').insertAdjacentHTML('afterbegin', template);
-        }
-    })
-})
-()
+const fetchPhotoData = async function() {
+    const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=1dqCkXhcDym7QqNzSb1hpgOjBndEiaOaoD7y2IwJ';
+    const res = await fetch(url);
+    const data = await res.json();
+    const spacePhotos = data.photos;
+
+    displayPhotoData(spacePhotos)
+}
+
+const displayPhotoData = async function(data) {
+    for (let i = 0; i < data.length; i++) {
+        let template = `
+        <div class="photo-card">
+            <img src="${data[i].img_src}" alt="${data[i].camera.full_name}">
+            <h2>${data[i].rover.name} Rover - ${data[i].camera.full_name}</h2>
+            <p>${data[i].earth_date}</p>
+            <button class="like-photo-${i}">Like</button>
+            <button class="share-photo-${i}">Share</button>
+        </div>
+        `
+        document.querySelector('main').insertAdjacentHTML('afterbegin', template); 
+    }
+}
+
+fetchPhotoData()
